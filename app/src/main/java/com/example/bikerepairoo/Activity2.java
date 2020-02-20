e3a2c4544819753a5f702891e351767fb81af639
kevinharten <kevinharten778@gmail.com>
2020-02-20 18:36:25 +0500

    Activity3 2/20/2020

--- app/src/main/java/com/example/bikerepairoo/Activity2.java
+++ app/src/main/java/com/example/bikerepairoo/Activity2.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -16,7 +19,7 @@ import com.google.android.material.textfield.TextInputLayout;
 public class Activity2 extends AppCompatActivity {
     private ImageView imageView2;
     private EditText email,pass;
-
+    private Button signUp;
     long duration = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,15 @@ public class Activity2 extends AppCompatActivity {
 
         email = (EditText) findViewById(R.id.email);
         pass = (EditText)findViewById(R.id.pass);
+        signUp = (Button)findViewById(R.id.SignUp);
 
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Activity2.this,Activity3.class);
+                startActivity(i);
+            }
+        });
 
 
 
--- app/src/main/java/com/example/bikerepairoo/Activity3.java
+++ app/src/main/java/com/example/bikerepairoo/Activity3.java
@@ -1,26 +1,92 @@
 package com.example.bikerepairoo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class Activity3 extends AppCompatActivity {
     private TextInputLayout mEmailLayout,mPasswordLayout,mUsernameLayout;
-    private Button buttonvalidate;
+    private Button mBtnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
 
         mEmailLayout = (TextInputLayout)findViewById(R.id.et_email);
         mPasswordLayout = (TextInputLayout)findViewById(R.id.et_password);
         mUsernameLayout = (TextInputLayout)findViewById(R.id.et_username);
-        buttonvalidate = (Button)findViewById(R.id.btn_validate);
+        mBtnSignIn = (Button)findViewById(R.id.btn_validate);
+
+        mBtnSignIn.setOnClickListener(this::submitData);
+
+        mEmailLayout.setErrorTextColor(ColorStateList.valueOf(Color.BLUE ));
 
 
 
+
+    }
+    private boolean validateEmailAdress() {
+        String email = mEmailLayout.getEditText().toString().trim();
+        if(email.isEmpty()) {
+            mEmailLayout.setError("Email is Required, Cant be Empty!");
+            return false;
+        }else {
+            mEmailLayout.setError(null);
+            return true;
+        }
+
+    }
+    private boolean validatePassword() {
+        String password = mPasswordLayout.getEditText().toString().trim();
+        if(password.isEmpty()) {
+            mPasswordLayout.setError("Password is Required, Cant be Empty!");
+            return false;
+        }else if(password.length()<12) {
+            mPasswordLayout.setError("Password is short, 12 Charachters are required");
+            return false;
+        }
+        else {
+            mPasswordLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        String username = mUsernameLayout.getEditText().toString().trim();
+        if(username.isEmpty()) {
+            mUsernameLayout.setError("Username is Required, Cant be Empty!");
+            return false;
+        }else if(username.length()<10){
+            mUsernameLayout.setError("Username is short,10 Charachters are required");
+            return true;
+        }
+        else {
+            mUsernameLayout.setError(null);
+            return true;
+        }
+    }
+
+
+    private void submitData(View view) {
+        if(!validateEmailAdress() || !validatePassword() || !validateUsername()) {
+            return;
+        }
+
+        String data = mEmailLayout.getEditText().getText().toString().trim() +"\n"+
+                mUsernameLayout.getEditText().getText().toString().trim()+"\n"+
+                mPasswordLayout.getEditText().getText().toString();
+
+        Toast.makeText(this,data, Toast.LENGTH_LONG).show();
     }
 }
