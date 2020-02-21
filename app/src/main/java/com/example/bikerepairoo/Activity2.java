34a63738356bd9708d038093299fca96268ea217
kevinharten <kevinharten778@gmail.com>
2020-02-21 10:50:20 +0500

    Updated 2/21/2020

--- app/src/main/java/com/example/bikerepairoo/Activity2.java
+++ app/src/main/java/com/example/bikerepairoo/Activity2.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.Gravity;
@@ -16,9 +18,12 @@ import android.widget.ImageView;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.w3c.dom.Text;
+
 public class Activity2 extends AppCompatActivity {
     private ImageView imageView2;
-    private EditText email,pass;
+   // private EditText email,password;
+    private TextInputLayout memail,mpassword;
     private Button signUp;
     long duration = 1000;
     @Override
@@ -29,9 +34,18 @@ public class Activity2 extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        email = (EditText) findViewById(R.id.email);
-        pass = (EditText)findViewById(R.id.pass);
+//        email = (EditText) findViewById(R.id.email);
+//        password = (EditText)findViewById(R.id.password);
+        memail=(TextInputLayout)findViewById(R.id.memail);
+        mpassword=(TextInputLayout)findViewById(R.id.mpassword);
         signUp = (Button)findViewById(R.id.SignUp);
+
+        memail.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+        mpassword.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+
+
+
+
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +61,39 @@ public class Activity2 extends AppCompatActivity {
 
 
 
+
         imageView2 = (ImageView)findViewById(R.id.imageView2);
 
         //Animator
-        ObjectAnimator objAnime = ObjectAnimator.ofFloat(imageView2,"y",250f);
+        ObjectAnimator objAnime = ObjectAnimator.ofFloat(imageView2,"y",195f);
         objAnime.setDuration(duration);
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(objAnime);
         animatorSet.start();
+
+
+    }
+
+    private boolean validateEmail() {
+        String email = memail.getEditText().getText().toString().trim();
+        if (email.isEmpty()){
+            memail.setError("Email is Required, Cant be Empty !");
+            return false;
+        } else {
+            memail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validePassword() {
+        String pass = mpassword.getEditText().getText().toString().trim();
+        if (pass.isEmpty()) {
+            mpassword.setError("Password is required, Cant be empty!");
+            return false;
+        }
+        else {
+            mpassword.setError(null);
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/bikerepairoo/Activity3.java
+++ app/src/main/java/com/example/bikerepairoo/Activity3.java
@@ -13,8 +13,9 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class Activity3 extends AppCompatActivity {
-    private TextInputLayout mEmailLayout,mPasswordLayout,mUsernameLayout;
-    private Button mBtnSignIn;
+    private TextInputLayout mEmailLayout, mPasswordLayout, mUsernameLayout, mFirstName, mLastName;
+    private Button mBtnNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,42 +24,68 @@ public class Activity3 extends AppCompatActivity {
         actionBar.hide();
 
 
-        mEmailLayout = (TextInputLayout)findViewById(R.id.et_email);
-        mPasswordLayout = (TextInputLayout)findViewById(R.id.et_password);
-        mUsernameLayout = (TextInputLayout)findViewById(R.id.et_username);
-        mBtnSignIn = (Button)findViewById(R.id.btn_validate);
+        mEmailLayout = (TextInputLayout) findViewById(R.id.et_email);
+        mPasswordLayout = (TextInputLayout) findViewById(R.id.et_password);
+        mUsernameLayout = (TextInputLayout) findViewById(R.id.et_username);
+        mFirstName = (TextInputLayout) findViewById(R.id.et_firstName);
+        mLastName = (TextInputLayout) findViewById(R.id.et_lastName);
+        mBtnNext = (Button) findViewById(R.id.btn_validate);
 
-        mBtnSignIn.setOnClickListener(this::submitData);
+        mBtnNext.setOnClickListener(this::submitData);
 
-        mEmailLayout.setErrorTextColor(ColorStateList.valueOf(Color.RED ));
+        mFirstName.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+        mLastName.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+        mEmailLayout.setErrorTextColor(ColorStateList.valueOf(Color.RED));
         mPasswordLayout.setErrorTextColor(ColorStateList.valueOf(Color.RED));
         mUsernameLayout.setErrorTextColor(ColorStateList.valueOf(Color.RED));
-
-
+        mFirstName.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+        mLastName.setErrorTextColor(ColorStateList.valueOf(Color.RED));
 
 
     }
+
+    private boolean validateFirstName() {
+        String firstName = mFirstName.getEditText().getText().toString().trim();
+        if (firstName.isEmpty()) {
+            mUsernameLayout.setError("First Name is required, Cant be Empty!");
+            return false;
+        } else {
+            mFirstName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateLastName() {
+        String lastName = mLastName.getEditText().getText().toString().trim();
+        if(lastName.isEmpty()) {
+            mLastName.setError("Last Name is required, Cant be Empty!");
+            return false;
+        } else{
+            mLastName.setError(null);
+            return true;
+        }
+    }
     private boolean validateEmailAdress() {
         String email = mEmailLayout.getEditText().getText().toString().trim();
-        if(email.isEmpty()) {
+        if (email.isEmpty()) {
             mEmailLayout.setError("Email is Required, Cant be Empty!");
             return false;
-        }else {
+        } else {
             mEmailLayout.setError(null);
             return true;
         }
 
     }
+
     private boolean validatePassword() {
         String password = mPasswordLayout.getEditText().getText().toString().trim();
-        if(password.isEmpty()) {
+        if (password.isEmpty()) {
             mPasswordLayout.setError("Password is Required, Cant be Empty!");
             return false;
-        }else if(password.length()<12) {
+        } else if (password.length() < 12) {
             mPasswordLayout.setError("Password is short, 12 Charachters are required");
             return false;
-        }
-        else {
+        } else {
             mPasswordLayout.setError(null);
             return true;
         }
@@ -66,29 +93,33 @@ public class Activity3 extends AppCompatActivity {
 
     private boolean validateUsername() {
         String username = mUsernameLayout.getEditText().getText().toString().trim();
-        if(username.isEmpty()) {
+        if (username.isEmpty()) {
             mUsernameLayout.setError("Username is Required, Cant be Empty!");
             return false;
-        }else if(username.length()<10){
+        } else if (username.length() < 10) {
             mUsernameLayout.setError("Username is short,10 Charachters are required");
             return true;
-        }
-        else {
+        } else {
             mUsernameLayout.setError(null);
             return true;
         }
     }
 
 
+
+
     private void submitData(View view) {
-        if(!validateEmailAdress() || !validatePassword() || !validateUsername()) {
+        if (!validateEmailAdress() || !validatePassword() || !validateUsername()) {
             return;
         }
 
-        String data = mEmailLayout.getEditText().getText().toString().trim() +"\n"+
-                mUsernameLayout.getEditText().getText().toString().trim()+"\n"+
-                mPasswordLayout.getEditText().getText().toString();
-
-        Toast.makeText(this,data, Toast.LENGTH_LONG).show();
+//        String data = mEmailLayout.getEditText().getText().toString().trim() + "\n" +
+////                mUsernameLayout.getEditText().getText().toString().trim() + "\n" +
+////                mPasswordLayout.getEditText().getText().toString();
+////
+////        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
+
+
 }
+
